8c5ede41e9c3c3dba6548b3328ccae8b2f15917d
danaDvlp <colleksql3@gmail.com>
2022-09-14 01:28:58 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- Remove & use example component! -->
+  <h1
+    :[attr]="status"
+    @[event]="add">
+    {{ difMessage }}
+  </h1>
+  
+  <HelloWorld 
+    :msg="message" 
+    @click="handler" />
+  <HelloWorld msg="Good" />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello Vue!',
+      difMessage: "Goodbye Vue!",
+      status: 'active',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+  methods: {
+    handler() {
+      console.log(this.message)
+      this.message += '!'
+    },
+    add() {
+      console.log(this.difMessage)
+      this.difMessage += '!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .active{
+    font-size: 40px;
+    color : royalblue;
+  }
+</style>
